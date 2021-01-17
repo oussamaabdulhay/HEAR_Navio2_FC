5e6a04ec8f6dbe68d4e73997741e824ce0107e18
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-01-17 18:33:33 +0400

    .

--- example_node/example_node.cpp
+++ example_node/example_node.cpp
@@ -168,7 +168,7 @@ int main(int argc, char** argv) {
     ActuationSystem* myActuationSystem = new HexaActuationSystem(actuators);
     #ifdef BIG_HEXA
     //myActuationSystem->setESCValues(1165 ,1000, 2000);
-    myActuationSystem->setESCValues(1165 ,0, 2000);
+    myActuationSystem->setESCValues(1165 ,100, 2000);
     #endif
     // ActuationSystem* myActuationSystem = new QuadActuationSystem(actuators);
 
