474096163a60f76ec1a3d8e2a3ee20f2083371db
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-06-17 13:10:16 +0400

    .

--- providers_camera_node/providers_camera_node.cpp
+++ providers_camera_node/providers_camera_node.cpp
@@ -126,14 +126,14 @@ int main(int argc, char **argv){
     camera_z_kalmanFilter->getPorts()[(int)KalmanFilter::ports_id::OP_0_POS]->connect(threshold_z->getPorts()[Threshold_status::ports_id::IP_1_KF]);
    
    
-    // threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_position_switch_x->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
-    // threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_position_switch_z->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
-    // threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_velocity_switch_x->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
-    // threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_velocity_switch_z->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
-    // threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(rosunit_switch_pds_x->getPorts()[(int)ROSUnit_SetFloatClnt::ports_id::IP_0]);
-    // threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(rosunit_switch_pds_z->getPorts()[(int)ROSUnit_SetFloatClnt::ports_id::IP_0]);
-    // threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(probe5->getPorts()[(int)ROSUnit_FloatPub::ports_id::IP_0]);
-    // threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(probe6->getPorts()[(int)ROSUnit_FloatPub::ports_id::IP_0]);
+    threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_position_switch_x->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
+    threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_position_switch_z->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
+    threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_velocity_switch_x->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
+    threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(mux_velocity_switch_z->getPorts()[(int)InvertedSwitch::ports_id::IP_1_TRIGGER]);
+    threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(rosunit_switch_pds_x->getPorts()[(int)ROSUnit_SetFloatClnt::ports_id::IP_0]);
+    threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(rosunit_switch_pds_z->getPorts()[(int)ROSUnit_SetFloatClnt::ports_id::IP_0]);
+    threshold_x->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(probe5->getPorts()[(int)ROSUnit_FloatPub::ports_id::IP_0]);
+    threshold_z->getPorts()[Threshold_status::ports_id::OP_0_HOV_TRACK]->connect(probe6->getPorts()[(int)ROSUnit_FloatPub::ports_id::IP_0]);
 
 
     //CAMERA X PROVIDER WITH KALMAN FILTER
